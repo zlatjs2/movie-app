887c3f050b7f73419ca95c8eafa2b7319fdcd96e
kimseonmi <zlatjs2@gmail.com>
2017-11-25 14:33:26 +0900

    리팩토링 준비 (중간 커밋)

--- README.md
+++ README.md
@@ -1,19 +1,1 @@
 # MovieApp
-
-## 이 강좌에서 배운것들
-
-### React.js로 API 데이터 가져오기
-
-__API__ : 지정된 엔드 포인트가 있는 JSON 형식의 데이터 세트
-
-__fetch__ : 데이터 요청 방법
-
-[참고](https://facebook.github.io/react-native/docs/network.html)
-
-__Promise__ : 비동기 계산을 위해 사용
-
-promise는 다음 중 하나의 상태(state)가 될 것이다.
-
-- 대기중(pending): 초기 상태, 이행 또는 거부되지 않은.
-- 이행됨(fulfilled): 연산이 성공리에 완료되었음을 뜻함
-- 거부됨(rejected): 연산이 실패했음을 뜻함
--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import React, { Component } from 'react';
-import MovieList from './components/MovieList';
-
 import './App.css';
 
 class App extends Component {
@@ -10,32 +8,11 @@ class App extends Component {
     this.state = {}
   }
 
-  componentDidMount() {
-    this._getMovies()
-  }
-
-  _getMovies = async() => {
-    const movies = await this._callApi();
-    this.setState({ movies });
-  }
-
-  _callApi = () => {
-    return fetch('https://yts.ag/api/v2/list_movies.json')
-          .then(response => response.json())
-          .then(json => json.data.movies)
-          .catch(err => console.log(err));
-  }
-
-  _renderMovies = () => {
-    const movies = <MovieList movies={this.state.movies} />
-    return movies;
-  }
-
   render() {
     return (
       <div className="app">
         <h1 className="app-title">MovieApp</h1>
-        {this.state.movies ? this._renderMovies() : <div>Loading..</div> }
+        test
       </div>
     );
   }
